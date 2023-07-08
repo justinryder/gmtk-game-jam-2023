620b6494430e5311014fa73f4d380a187863283f
Justin Ryder <jlr2383@gmail.com>
2023-07-08 14:31:27 -0700

    asdf

--- GMTKGameJam2023/Assets/Scripts/ShipHealth.cs
+++ GMTKGameJam2023/Assets/Scripts/ShipHealth.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class ShipHealth : MonoBehaviour
 {
-    private int _health = 5;
+    private HealthBar HealthBar;
+
+    private const int _maxHealth = 1;
+    private int _health = _maxHealth;
+
+    void Awake()
+    {
+        HealthBar = GameObject.FindWithTag(Game.HealthBarTag)?.GetComponent<HealthBar>();
+        if (!HealthBar)
+        {
+            Debug.LogError("ShipHealth could not find a HealthBar tagged game object with HealthBar script");
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp("z"))
+        {
+            OnHit();
+        }
+    }
 
     public void OnHit()
     {
-        Debug.Log("Ship hit by asteroid, -1 hp");
+        // Debug.Log("Ship hit by asteroid, -1 hp");
         _health -= 1;
+
+        if (HealthBar)
+        {
+            HealthBar.SetHealth((float)_health / (float)_maxHealth);
+        }
 
         if (_health <= 0)
         {
@@ -20,7 +45,13 @@ public class ShipHealth : MonoBehaviour
     void Die()
     {
         // TODO: show win screen on ship death
-        Debug.Log("Ship was destroyed! Show win screen");
-        Destroy(gameObject);
+        // Debug.Log("Ship was destroyed! Show win screen");
+        // gameObject.SetActive(false);
+
+        var game = GameObject.FindWithTag("Game")?.GetComponent<Game>();
+        if (game)
+        {
+            game.Win();
+        }
     }
 }
