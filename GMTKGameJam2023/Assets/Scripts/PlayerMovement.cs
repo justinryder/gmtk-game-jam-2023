c61b75caad98bce9fd77fb16fbbcc35465716188
ksibdesign <ksibdesign@gmail.com>
2023-07-08 01:08:08 -0400

    Tags for Player Movement

---
+++ GMTKGameJam2023/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private GameObject SOI;
+    private GameObject Ship;
+    private GameObject[] Asteroid;
+    public float distanceTotal;
+
+    private float speed = -1.0f;
+
+    void Awake()
+    {
+        SOI = GameObject.FindWithTag("SOI");
+        if (!SOI)
+        {
+            Debug.LogError("PlayerMovement.cs requires the tag SOI to exist in the scene");
+        }
+        Asteroid = GameObject.FindGameObjectsWithTag("Asteroid");
+        if (!Asteroid)
+        {
+            Debug.LogError("PlayerMovement.cs requires the tag Asteroid to exist in the scene");
+        }
+        
+        Ship = GameObject.FindWithTag("Ship");
+        if (!Ship)
+        {
+            Debug.LogError("PlayerMovement.cs requires the tag Ship to exist in the scene");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach ()
+        {
+
+        }
+        Vector3 delta = SOI.transform.position - Ship.transform.position;
+        distanceTotal = delta.magnitude;
+
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position += delta.normalized * step;
+    }
+}
