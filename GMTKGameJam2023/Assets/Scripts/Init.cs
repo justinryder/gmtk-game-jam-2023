d91f33cd4da72d480e97fa653199b5a0c839a496
Justin Ryder <jlr2383@gmail.com>
2023-07-08 19:38:07 -0700

    build settings

---
+++ GMTKGameJam2023/Assets/Scripts/Init.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    void Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
